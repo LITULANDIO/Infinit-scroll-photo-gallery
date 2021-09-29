0efd953422299fa19d0ecb61bd5664cfb95caf8d
litus <litulandio@gmail.com>
2021-09-29 17:44:09 +0200

    add cmp Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
+  <Navbar name="Carles Far"/>
   <Home />
 </template>
 
 <script>
 import Home from '@/views/Home.vue';
+import Navbar from '@/components/navbar/Navbar.vue';
+
 export default {
   name: 'App',
   components: {
-    Home
+    Home,
+    Navbar
   }
 }
 </script>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,32 @@
+<template src="./Navbar.html"></template>
+
+<script>
+import { onMounted, ref } from 'vue';
+
+export default {
+    name: 'Navbar',
+    props:{
+        name:{
+            type: String
+        }
+    },
+    setup(){
+        const date = ref();
+
+        const getDate = () => {
+            const dateNow = new Date();
+            return date.value = `${dateNow.getDate()}/${dateNow.getMonth() + 1}/${dateNow.getFullYear()}`;
+        }
+
+        onMounted(() =>{
+            getDate();
+        })
+
+        return{
+            date
+        }
+    }
+}
+</script>
+
+<style lang="scss" src="./Navbar.scss" scoped></style>
