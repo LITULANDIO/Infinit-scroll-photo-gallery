020f5b466663d08cb76a028e49bcbee89893fa87
litus <litulandio@gmail.com>
2021-09-30 10:55:38 +0200

    add cmp photo

---
+++ src/components/photo/Photo.vue
@@ -0,0 +1,23 @@
+<template src="./Photo.html"></template>
+
+<script>
+export default {
+    name: 'Photo',
+    props:{
+        photo:{
+            type: Object
+        }
+
+    },
+    setup(){
+
+
+        return{
+
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" src="./Photo.scss" scoped></style>
